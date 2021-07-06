428a29cb84dbed62531009e14f6141f3c7a623de
Nabeel Asif <nabeel.asif362@gmail.com>
2021-07-06 19:02:58 +0500

    add cs files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Csharp_ConsoleApp_Basics
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Say Hello to Basics");
+        }
+    }
+}
