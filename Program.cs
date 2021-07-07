309d4a48ab1bec0f91e92236865ba67415d5c872
Nabeel Asif <nabeel.asif362@gmail.com>
2021-07-07 12:51:10 +0500

    add concatenation and placeholder methods to see working

--- Program.cs
+++ Program.cs
@@ -7,6 +7,19 @@ namespace Csharp_ConsoleApp_Basics
         static void Main()
         {
             Console.WriteLine("Say Hello to Basics");
+
+            // Reading and Write to Console 
+            Console.WriteLine("Enter your first name :");
+            string FirstName = Console.ReadLine();
+
+            Console.WriteLine("Enter your last name :");
+            string LastName = Console.ReadLine();
+            
+            // Printing Full Name using concatenation method :
+            Console.WriteLine("Full Name is = " + FirstName + " " + LastName);
+
+            // Printing Full Name using Placeholder method :     ** This method is mostly preffered !
+            Console.WriteLine("Complete Name is : {0} - {1}", FirstName, LastName);
         }
     }
 }
