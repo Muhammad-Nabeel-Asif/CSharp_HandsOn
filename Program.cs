af6e3e63ddd8a9caf6c327190b99ddeb715aa154
Nabeel Asif <nabeel.asif362@gmail.com>
2021-07-08 22:08:52 +0500

    add Verbatim literal

--- Program.cs
+++ Program.cs
@@ -12,14 +12,22 @@ namespace Csharp_ConsoleApp_Basics
             Console.WriteLine("Enter your first name :");
             string FirstName = Console.ReadLine();
 
+
             Console.WriteLine("Enter your last name :");
             string LastName = Console.ReadLine();
             
+
             // Printing Full Name using concatenation method :
             Console.WriteLine("Full Name is = " + FirstName + " " + LastName);
 
+
             // Printing Full Name using Placeholder method :     ** This method is mostly preffered !
             Console.WriteLine("Complete Name is : {0} - {1}", FirstName, LastName);
+
+
+            // Using verbatim string/literal (@) with "strings":
+            string address = @"House # 13 \ Street A \ Housing Colony # 1 \ TTS";
+            Console.WriteLine(address);
         }
     }
 }
