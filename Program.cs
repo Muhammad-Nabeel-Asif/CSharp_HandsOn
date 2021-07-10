2b726cde8ca0592d54cab75f6aa04ba73620f270
Nabeel Asif <nabeel.asif362@gmail.com>
2021-07-10 21:20:27 +0500

    explicit typecasting and int.Parse() and int.TryParse() methods

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Csharp_ConsoleApp_Basics
     {
         static void Main()
         {
-            
+
             Console.WriteLine("Say Hello to Basics");
 
             // => Reading and Write to Console 
@@ -33,12 +33,44 @@ namespace Csharp_ConsoleApp_Basics
 
             // => nullable data types and use of null coalescing operator : 
             // converting a non-nullable datatype to nullable using symbol "?" on next line :
-            int? ticketsOnSale = null;
+            int? ticketsOnSale = 5600;
             // using null coalescing operator >> if value of ticketOnSale is null/zero then assign zero to that variable , otherwise assign the default value to the respective variable i.e "ticketsOnSale" in this example context : 
             int availableTickets = ticketsOnSale ?? 0;
 
             Console.WriteLine("Available tickets are = {0}", availableTickets);
 
+
+            // => Different types of explicit typecasting , implicit are done automatically by the compiler :
+
+            // using  cast () operator conversion =>
+             float f = 123456.789F;
+            // int i = (int)f;
+
+            // or use convert class => , this method is preffered as it throw exceptions if there is a problem !
+            int i = Convert.ToInt32(f);
+
+
+            // => Parse and TryParse methods to convert strings to int types :
+
+            // int.Parse() method to convert a digiti string to a int datatype :
+
+            string strNumber = "500";
+            int result = int.Parse(strNumber);
+            Console.WriteLine(result);
+
+            // int.TryParse method to return true or false on successfull or failed conversion :
+
+            string strNumber1 = "123";
+            int result1 = 0;
+            bool isConversionSuccessfull = int.TryParse(strNumber1, out result1);
+
+            if (isConversionSuccessfull)
+            {
+                Console.WriteLine(result1);
+            } else
+            {
+                Console.WriteLine("Please Enter a Valid Number !");
+            }
         }
     }
 }
