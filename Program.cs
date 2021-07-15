2c12cc8aa1c3bb7bc49e465b69d2be13df6c235b
Nabeel Asif <nabeel.asif362@gmail.com>
2021-07-15 19:09:52 +0500

    uncomment program lines

--- Program.cs
+++ Program.cs
@@ -7,135 +7,137 @@ namespace Csharp_ConsoleApp_Basics
         static void Main()
         {
 
-            //Console.WriteLine("Say Hello to Basics");
+            Console.WriteLine("Say Hello to Basics");
 
-            //// (1)=> Reading and Write to Console 
-            //Console.WriteLine("Enter your first name :");
-            //string FirstName = Console.ReadLine();
-
-
-            //Console.WriteLine("Enter your last name :");
-            //string LastName = Console.ReadLine();
+            // (1)=> Reading and Write to Console 
+            Console.WriteLine("Enter your first name :");
+            string FirstName = Console.ReadLine();
 
 
-            //// (2)=> Printing Full Name using concatenation method :
-            //Console.WriteLine("Full Name is = " + FirstName + " " + LastName);
+            Console.WriteLine("Enter your last name :");
+            string LastName = Console.ReadLine();
 
 
-            //// (3)=> Printing Full Name using Placeholder method :     ** This method is mostly preffered !
-            //Console.WriteLine("Complete Name is : {0} - {1}", FirstName, LastName);
+            // (2)=> Printing Full Name using concatenation method :
+            Console.WriteLine("Full Name is = " + FirstName + " " + LastName);
 
 
-            //// (4)=> Using verbatim string/literal (@) with "strings":
-            //string address = @"House # 13 \ Street A \ Housing Colony # 1 \ TTS";
-            //Console.WriteLine(address);
+            // (3)=> Printing Full Name using Placeholder method :     ** This method is mostly preffered !
+            Console.WriteLine("Complete Name is : {0} - {1}", FirstName, LastName);
 
 
-            //// (5)=> nullable data types and use of null coalescing operator : 
-            //// converting a non-nullable datatype to nullable using symbol "?" on next line :
-            //int? ticketsOnSale = 5600;
-            //// using null coalescing operator >> if value of ticketOnSale is null/zero then assign zero to that variable , otherwise assign the default value to the respective variable i.e "ticketsOnSale" in this example context : 
-            //int availableTickets = ticketsOnSale ?? 0;
-
-            //Console.WriteLine("Available tickets are = {0}", availableTickets);
+            // (4)=> Using verbatim string/literal (@) with "strings":
+            string address = @"House # 13 \ Street A \ Housing Colony # 1 \ TTS";
+            Console.WriteLine(address);
 
 
-            //// (6)=> Different types of explicit typecasting , implicit are done automatically by the compiler :
+            // (5)=> nullable data types and use of null coalescing operator : 
+            // converting a non-nullable datatype to nullable using symbol "?" on next line :
+            int? ticketsOnSale = 5600;
+            // using null coalescing operator >> if value of ticketOnSale is null/zero then assign zero to that variable , otherwise assign the default value to the respective variable i.e "ticketsOnSale" in this example context : 
+            int availableTickets = ticketsOnSale ?? 0;
 
-            //// using  cast () operator conversion =>
-            // float f = 123456.789F;
-            //int i = (int)f;
-
-            //// or use convert class => , this method is preffered as it throw exceptions if there is a problem !
-            //int ii = Convert.ToInt32(f);
-
-
-            //// (7)=> Parse and TryParse methods to convert strings to int types :
-
-            //// int.Parse() method to convert a digiti string to a int datatype :
-
-            //string strNumber = "500";
-            //int result = int.Parse(strNumber);
-            //Console.WriteLine(result);
-
-            //// int.TryParse method to return true or false on successfull or failed conversion :
-
-            //string strNumber1 = "123";
-            //int result1 = 0;
-            //bool isConversionSuccessfull = int.TryParse(strNumber1, out result1);
-
-            //if (isConversionSuccessfull)
-            //{
-            //    Console.WriteLine(result1);
-            //} else
-            //{
-            //    Console.WriteLine("Please Enter a Valid Number !");
-            //}
+            Console.WriteLine("Available tickets are = {0}", availableTickets);
 
 
-            //// (8)=> Working with Arrays :
+            // (6)=> Different types of explicit typecasting , implicit are done automatically by the compiler :
 
-            //// first method ( not preffered )
+            // using  cast () operator conversion =>
+            float f = 123456.789F;
+            int i = (int)f;
 
-            //int[] marks = new int[3];
-            //marks[0] = 95;
-            //marks[1] = 90;
-            //marks[2] = 85;
-            //Console.WriteLine(marks[0]);
+            // or use convert class => , this method is preffered as it throw exceptions if there is a problem !
+            int ii = Convert.ToInt32(f);
 
-            //// second method ( preffered )
 
-            //int[] marks1 = new int[3] { 50, 60, 70 };
-            //Console.WriteLine(marks1[1]);
+            // (7)=> Parse and TryParse methods to convert strings to int types :
 
-            //// third method ( mostly preffered )
-            //int[] marks2 = {40,30,50};
-            //for (int i = 0; i < marks2.Length; i++)
-            //{
-            //    Console.WriteLine(marks2[i]);
-            //}
+            // int.Parse() method to convert a digiti string to a int datatype :
+
+            string strNumber = "500";
+            int result = int.Parse(strNumber);
+            Console.WriteLine(result);
+
+            // int.TryParse method to return true or false on successfull or failed conversion :
+
+            string strNumber1 = "123";
+            int result1 = 0;
+            bool isConversionSuccessfull = int.TryParse(strNumber1, out result1);
+
+            if (isConversionSuccessfull)
+            {
+                Console.WriteLine(result1);
+            }
+            else
+            {
+                Console.WriteLine("Please Enter a Valid Number !");
+            }
+
+
+            // (8)=> Working with Arrays :
+
+            // first method ( not preffered )
+
+            int[] marks = new int[3];
+            marks[0] = 95;
+            marks[1] = 90;
+            marks[2] = 85;
+            Console.WriteLine(marks[0]);
+
+            // second method ( preffered )
+
+            int[] marks1 = new int[3] { 50, 60, 70 };
+            Console.WriteLine(marks1[1]);
+
+            // third method ( mostly preffered )
+            int[] marks2 = { 40, 30, 50 };
+            for (int i = 0; i < marks2.Length; i++)
+            {
+                Console.WriteLine(marks2[i]);
+            }
 
 
             // (9)=> Working with if_else and switch statements :
 
-            //Console.WriteLine("Please Enter a number :");
-            //int userNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please Enter a number :");
+            int userNumber = int.Parse(Console.ReadLine());
+
             // if_else statements :
 
-            //if (userNumber == 10)
-            //{
-            //    Console.WriteLine("you entered number :  {0}", userNumber);
-            //}
-            //else if (userNumber == 20)
-            //{
-            //    Console.WriteLine("you entered number :  {0}", userNumber);
-            //}
-            //else if (userNumber == 30)
-            //{
-            //    Console.WriteLine("you entered number :  {0}", userNumber);
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Please Enter a valid number");
-            //}
+            if (userNumber == 10)
+            {
+                Console.WriteLine("you entered number :  {0}", userNumber);
+            }
+            else if (userNumber == 20)
+            {
+                Console.WriteLine("you entered number :  {0}", userNumber);
+            }
+            else if (userNumber == 30)
+            {
+                Console.WriteLine("you entered number :  {0}", userNumber);
+            }
+            else
+            {
+                Console.WriteLine("Please Enter a valid number");
+            }
 
-            //  switch statements :
+            // switch statements :
 
-            //switch(userNumber)
-            //{
-            //    case 10:
-            //        Console.WriteLine("you entered number :  {0}", userNumber);
-            //        break;
-            //    case 20:
-            //        Console.WriteLine("you entered number :  {0}", userNumber);
-            //        break;
-            //    case 30:
-            //        Console.WriteLine("you entered number :  {0}", userNumber);
-            //        break;
-            //    default:
-            //        Console.WriteLine("Please Enter a valid number");
-            //        break;
-            //}
+            switch (userNumber)
+            {
+                case 10:
+                    Console.WriteLine("you entered number :  {0}", userNumber);
+                    break;
+                case 20:
+                    Console.WriteLine("you entered number :  {0}", userNumber);
+                    break;
+                case 30:
+                    Console.WriteLine("you entered number :  {0}", userNumber);
+                    break;
+                default:
+                    Console.WriteLine("Please Enter a valid number");
+                    break;
+            }
 
 
             // Program to prompt user to console and select any coffee size from 1-3 and calculate its price , sum all of them if purchased yet again and show final bill :
