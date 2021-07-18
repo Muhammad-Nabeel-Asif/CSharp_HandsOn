b358e18a98a04b0c6bdd0b5409bb5338fbeb76aa
Nabeel Asif <nabeel.asif362@gmail.com>
2021-07-18 14:28:28 +0500

    add sec - 10

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ namespace Csharp_ConsoleApp_Basics
             Console.WriteLine("Enter your first name :");
             string FirstName = Console.ReadLine();
 
+            // ---------------------------------------------------------------------------------------------------------------------------------
 
             Console.WriteLine("Enter your last name :");
             string LastName = Console.ReadLine();
@@ -21,15 +22,18 @@ namespace Csharp_ConsoleApp_Basics
             // (2)=> Printing Full Name using concatenation method :
             Console.WriteLine("Full Name is = " + FirstName + " " + LastName);
 
+            // ---------------------------------------------------------------------------------------------------------------------------------
 
             // (3)=> Printing Full Name using Placeholder method :     ** This method is mostly preffered !
             Console.WriteLine("Complete Name is : {0} - {1}", FirstName, LastName);
 
+            // ---------------------------------------------------------------------------------------------------------------------------------
 
             // (4)=> Using verbatim string/literal (@) with "strings":
             string address = @"House # 13 \ Street A \ Housing Colony # 1 \ TTS";
             Console.WriteLine(address);
 
+            // ---------------------------------------------------------------------------------------------------------------------------------
 
             // (5)=> nullable data types and use of null coalescing operator : 
             // converting a non-nullable datatype to nullable using symbol "?" on next line :
@@ -39,6 +43,7 @@ namespace Csharp_ConsoleApp_Basics
 
             Console.WriteLine("Available tickets are = {0}", availableTickets);
 
+            // ---------------------------------------------------------------------------------------------------------------------------------
 
             // (6)=> Different types of explicit typecasting , implicit are done automatically by the compiler :
 
@@ -49,6 +54,7 @@ namespace Csharp_ConsoleApp_Basics
             // or use convert class => , this method is preffered as it throw exceptions if there is a problem !
             int ii = Convert.ToInt32(f);
 
+            // ---------------------------------------------------------------------------------------------------------------------------------
 
             // (7)=> Parse and TryParse methods to convert strings to int types :
 
@@ -73,6 +79,7 @@ namespace Csharp_ConsoleApp_Basics
                 Console.WriteLine("Please Enter a Valid Number !");
             }
 
+            // ---------------------------------------------------------------------------------------------------------------------------------
 
             // (8)=> Working with Arrays :
 
@@ -96,6 +103,7 @@ namespace Csharp_ConsoleApp_Basics
                 Console.WriteLine(marks2[index]);
             }
 
+            // ---------------------------------------------------------------------------------------------------------------------------------
 
             // (9)=> Working with if_else and switch statements :
 
@@ -139,22 +147,23 @@ namespace Csharp_ConsoleApp_Basics
                     break;
             }
 
+            // ---------------------------------------------------------------------------------------------------------------------------------
 
             // Program to prompt user to console and select any coffee size from 1-3 and calculate its price , sum all of them if purchased yet again and show final bill :
 
             int totalCoffeePrice = 0;
 
-            StartLabel:
+        StartLabel:
             Console.WriteLine("Please Select a Coffee size to buy : 1 - small , 2 - medium , 3 - large ");
             int userChoice = int.Parse(Console.ReadLine());
 
-            switch(userChoice)
+            switch (userChoice)
             {
                 case 1:
                     totalCoffeePrice += 1;
                     break;
                 case 2:
-                    totalCoffeePrice += 2;                    
+                    totalCoffeePrice += 2;
                     break;
                 case 3:
                     totalCoffeePrice += 3;
@@ -164,7 +173,7 @@ namespace Csharp_ConsoleApp_Basics
                     goto StartLabel;
             }
 
-            decideLabel:
+        decideLabel:
             Console.WriteLine("\n Do you want to purchase more coffee ? Press 'Y' for yes and 'N' for no");
             string userDecision = Console.ReadLine().ToUpper();
 
@@ -180,6 +189,77 @@ namespace Csharp_ConsoleApp_Basics
             }
             Console.WriteLine("Thank You for shopping with us , We hope to see you again :");
             Console.WriteLine("\n Your bill for coffee is : {0}\n", totalCoffeePrice);
-        }   
+
+            // ---------------------------------------------------------------------------------------------------------------------------------
+
+            // (10)=> Working with do_while , for and foreach loops :
+            // Program to prompt user to console and select any coffee size from 1-3 and calculate its price , sum all of them if purchased yet again and show final bill :
+
+
+            int totalBill1 = 0;
+            string userDecision1 = string.Empty;
+
+            do
+            {
+                int userChoice1 = 0;
+                do
+                {
+                    Console.WriteLine("Welcome to Coffee Arena , Please Select a coffee size from Menu");
+                    Console.WriteLine("1 - for small  , 2 - for medium , 3 - for large");
+                    userChoice1 = int.Parse(Console.ReadLine());
+
+                    switch (userChoice1)
+                    {
+                        case 1:
+                            totalBill1 += 10;
+                            break;
+                        case 2:
+                            totalBill1 += 20;
+                            break;
+                        case 3:
+                            totalBill1 += 30;
+                            break;
+                        default:
+                            Console.WriteLine("Your choice {0} is invalid", userChoice1);
+                            break;
+                    }
+                } while (userChoice1 != 1 && userChoice1 != 2 && userChoice1 != 3);
+
+                do
+                {
+                    Console.WriteLine("Do you want to buy another coffee - yes or no ?");
+                    userDecision1 = Console.ReadLine().ToLower();
+
+                    if (userDecision1 != "yes" && userDecision1 != "no")
+                    {
+                        Console.WriteLine("Your choice {0} is Invalid. Please try again...", userDecision1);
+                    }
+                } while (userDecision != "yes" && userDecision != "no");
+
+            } while (userDecision1.ToLower() != "no");
+
+            Console.WriteLine("Thank you for shopping with us \n Your total Bill is {0} . ", totalBill1);
+
+            // ---------------------------------------------------------------------------------------------------------------------------------
+            // foreach and for loops => 
+
+            void loops()
+            {
+                int[] Numbers = new int[] {50,100,150,200,250,300};                
+
+                for (int j = 0; j<Numbers.Length; j++)
+                {
+                    Console.WriteLine(Numbers[j]);
+                }
+
+                foreach (int i in Numbers)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            // executing loops function here :
+            loops();
+        }
     }
 }
